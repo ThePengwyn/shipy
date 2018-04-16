545d29d893749103ab05061522d794e2d5b2a6bc
ThePengwyn <mlwgraham1992@gmail.com>
2018-04-16 10:52:49 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 18:10:53 2018
+
+@author: Peng
+"""
+
+from steuptools import setup
+
+setup(
+      name = 'shipy',
+      version = '0.01',
+      description = 'Multivariant Shannon Information',
+      author = 'Pengwyn',
+      author_email = '@',
+      url = '',
+      packages = ['shipy'],
+      install_requires = [
+              'numpy',
+              'scipy'
+              ]
+      )
---
+++ shipy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 13:15:28 2018
+
+@author: Peng
+"""
+
